1b2395d7fd1a855b14acc51aaf331b16c8cc9978
issalait <issalait@gmail.com>
2022-09-08 23:22:55 +0300

    get text from api

--- src/test/java/HelloWorldTest.java
+++ src/test/java/HelloWorldTest.java
@@ -1,8 +1,22 @@
 import org.junit.jupiter.api.Test;
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
 
 public class HelloWorldTest {
     @Test
     public void testHelloWorld(){
-        System.out.println("Hello world!");
+       Response response = RestAssured
+               .get("https://playground.learnqa.ru/api/hello")
+               .andReturn();
+       response.prettyPrint();
+
+    }
+
+    @Test
+    public void getTextFromApi(){
+        Response response = RestAssured
+                .get("https://playground.learnqa.ru/api/get_text")
+                .andReturn();
+        response.prettyPrint();
     }
 }
